54d101193c53ac0cea056db17597c6ac7746bbd6
DavidMon7 <david.montenegro.a07@gmail.com>
2025-04-20 13:49:20 -0500

    Delete game_assets.py

--- game_assets.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-# Cargar imágenes de assets
-def load_images():
-    player_image = pygame.image.load("assets/player.png")
-    obstacle_image = pygame.image.load("assets/obstacle.png")
-    coin_image = pygame.image.load("assets/coin.png")
-    return player_image, obstacle_image, coin_image
